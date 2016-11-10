3d8047dbc180204a0f383d8d85f6be83fe3541b4
Jololio <jala9132@740s-pl-120.local>
2016-11-10 13:30:39 -0700

    Created Project

---
+++ src/hipster/controller/HipsterController.java
@@ -0,0 +1,61 @@
+package hipster.controller;
+
+import hipster.model.Hipster;
+import hipster.view.HipsterFrame;
+
+
+public class HipsterController 
+{
+	private HipsterFrame appFrame;
+	private int [] numbers;
+	private String [] words = {"This ","is the ","second hipster","level: ","initialization ","lists"};
+	private Hipster [] hipsters;
+	
+	public HipsterController()
+	{
+		thirdLevelHipster();
+		hipsters = new Hipster[6];
+		appFrame = new HipsterFrame(this);
+	}
+	
+	private void thirdLevelHipster()
+	{
+		numbers = new int [] {0,1,2,3,4,5,6,7,8,9,0,9,8,7,6,5,4,3,2,1,0};
+	}
+	
+	public void start()
+	{
+		for(int index = 0; index < hipsters.length; index++)
+		{
+			hipsters[index] = new Hipster("Hipster # " + index);
+		}
+		
+		for(Hipster currentHipster : hipsters)
+		{
+			System.out.print(currentHipster + ", ");
+		}
+		System.out.println("Next");
+		for(String currentWord : words)
+		{
+			System.out.print(currentWord + ", ");
+		}
+		System.out.println("Next");
+		for(int tempInt : numbers)
+		{
+			System.out.print(tempInt + ", ");
+		}
+	}
+	
+	public String[] getWords()
+	{
+		return words;
+	}
+	public int[] getNumbers()
+	{
+		return numbers;
+	}
+	public Hipster[] getHipsters()
+	{
+		return hipsters;
+	}
+}
---
+++ src/hipster/view/HipsterPanel.java
@@ -0,0 +1,63 @@
+package hipster.view;
+
+import java.awt.event.*;
+import java.awt.*;
+import javax.swing.*;
+import hipster.controller.HipsterController;
+
+
+
+public class HipsterPanel extends JPanel
+{
+	private HipsterController baseController;
+	private JButton myButton;
+	private JComboBox dropDown;
+	private JLabel wordsLabel;
+	private SpringLayout baseLayout;
+	
+	
+	public HipsterPanel(HipsterController baseController)
+	{
+		super();
+		this.baseController = baseController;
+		myButton = new JButton("Click here!!");
+		wordsLabel = new JLabel("such words!");
+		baseLayout = new SpringLayout();
+		dropDown = new JComboBox(baseController.getWords());
+		
+		
+		setupPanel();
+		setupLayout();
+		setupListeners();
+	}
+	
+	private void setupPanel()
+	{
+		this.setLayout(baseLayout);
+		this.setBackground(Color.ORANGE);
+		this.add(myButton);
+		this.add(wordsLabel);
+		this.add(dropDown);
+	}
+	private void setupLayout()
+	{
+		baseLayout.putConstraint(SpringLayout.NORTH, dropDown, 6, SpringLayout.SOUTH, myButton);
+		baseLayout.putConstraint(SpringLayout.WEST, dropDown, 112, SpringLayout.WEST, this);
+		baseLayout.putConstraint(SpringLayout.NORTH, myButton, 70, SpringLayout.NORTH, this);
+		baseLayout.putConstraint(SpringLayout.WEST, myButton, 0, SpringLayout.WEST, this);
+		baseLayout.putConstraint(SpringLayout.NORTH, wordsLabel, 5, SpringLayout.NORTH, myButton);
+		baseLayout.putConstraint(SpringLayout.WEST, wordsLabel, 20, SpringLayout.EAST, myButton);
+	}
+	private void setupListeners()
+	{
+		dropDown.addActionListener(new ActionListener()
+		{
+			public void actionPerformed(ActionEvent selection)
+			{
+				String selectedText = baseController.getWords()[dropDown.getSelectedIndex()];
+				wordsLabel.setText(selectedText);
+			}
+		});
+		
+	}
+}
